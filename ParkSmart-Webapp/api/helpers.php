8688595d8f67846b8616a390bbef2b4ce3e479d3
Bryan Brauchler <bbrauchl@umich.edu>
2020-02-18 14:34:36 -0500

    adding pull API

--- ParkSmart-Webapp/api/helpers.php
+++ ParkSmart-Webapp/api/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-function get_sql_connection_info($var) {
+function get_sql_connection_info() {
     $result = [];
     $result['sql_servername'] = array_key_exists("sql_servername", $_POST) ? filter_var($_POST["sql_servername"], FILTER_SANITIZE_URL) : "localhost";
     $result['sql_username'] = array_key_exists("sql_username", $_POST) ? filter_var($_POST["sql_username"], FILTER_SANITIZE_STRING) : "ParkSmart";
@@ -17,7 +17,7 @@ function get_sql_connection($var) {
                         $var['sql_database']);
     
     if ($conn->connect_error) {
-        die("Connection Failed: ".$conn->connect_error);
+        throw new Exception("Could not connect to SQL database");
     }
     return $conn;
 }
@@ -39,6 +39,28 @@ function get_current_url() {
         
     // Print the link 
     return $url; 
+}
+
+function is_JSON_string($string){
+    return is_string($string) && json_decode($string, true) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
+ }
+
+function get_sql_all_tables() {
+    $conn_params = get_sql_connection_info();
+    $conn = get_sql_connection($conn_params);
+    $sql = "SHOW TABLES;";  
+    $result = $conn->query($sql);
+    if(!$result) {
+        throw new Exception("SQL Update Failed");
+    }
+    while($row = $result->fetch_assoc()) {
+        foreach($row as $key => $value) {
+            $resultArray[] = $value;
+        }
+    }     
+    $result->close(); 
+    $conn->close();
+    return $resultArray;
 }
 
 ?>
---
+++ ParkSmart-Webapp/api/pull.php
@@ -0,0 +1,60 @@
+<?php
+
+include "helpers.php";
+
+// if the request was made using the 'POST' method
+if ($_SERVER['REQUEST_METHOD']  === 'POST') {
+
+    $sql_server = get_sql_connection_info($_POST);
+
+    //get the names of all tables
+    $all_tables = get_sql_all_tables();
+
+    //check if the lot name exists
+    if (array_key_exists("Lot", $_POST)) {
+        if (is_JSON_string($_POST["Lot"])) {
+            $lots = json_decode($_POST["Lot"]);
+        } else {
+            $lots = array($_POST["Lot"]);
+        }
+    } else {
+        $lots = $all_tables;
+    }
+
+    $response = new stdClass();
+
+    $conn_info = get_sql_connection_info();
+    $conn = get_sql_connection($conn_info);
+
+    foreach ($lots as $lot) {
+        if(gettype($lot) != "string") {
+            $conn->close();
+            die("Error! Lot name specified in incorrect format. Should be provided as a string.");
+        }
+        if(!in_array(strtolower($lot), $all_tables)) {
+            $conn->close();
+            die("Error! User specified Parking lot name that does not exist in the database!");
+        }
+        $sql = "SELECT * FROM ".strtolower($lot)." WHERE start_timestamp <= NOW() && end_timestamp >= NOW();"; //todo: add "where" parameter to get the correct time entries
+        $result = $conn->query($sql); 
+        if(!$result) {
+            $conn->close();
+            die("SQL Update Failed");
+        }
+        while($row = $result->fetch_assoc()) {
+          $resultArray[] = $row;
+        }     
+        $response->{$lot} = $resultArray;
+        $result->close();
+    }
+    $conn->close();
+    
+    //success!!
+    echo json_encode($response);
+}
+else {
+    header("Location: " . str_replace(".php", ".html", get_current_url()));
+    die();
+}
+
+?>
