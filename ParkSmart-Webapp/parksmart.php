e7af5a3ca0647a4aae886888d37e240f9a6cc6d2
Bryan Brauchler <bbrauchl@umich.edu>
2020-02-22 23:54:41 -0500

    Adding React to project

--- ParkSmart-Webapp/parksmart.php
+++ ParkSmart-Webapp/parksmart.php
@@ -1,117 +1,38 @@
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+  <head>
+    <style>
+      table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+      }
 
-td, th {
-  border: 1px solid black;
-  text-align: left;
-  padding: 8px;
-}
+      td, th {
+        border: 1px solid black;
+        text-align: left;
+        padding: 8px;
+      }
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-</head>
-<body>
+      tr:nth-child(even) {
+        background-color: #dddddd;
+      }
+    </style>
+  </head>
+  <script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+  <script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+  <body>
+  
+    <div id="root"></div>
 
-<form method="get">
-<input type="text" id="host" name="host" value=<?= array_key_exists("host", $_GET) ? $_GET["host"] : ""; ?> >
-<input type="submit" value="Go"/>
-<input type="submit" name="history" value="Show All History"/>
-</form>
-
-<script type="text/javascript">
-function findGetParameter(parameterName) {
-    var result = null,
-        tmp = [];
-    location.search
-        .substr(1)
-        .split("&")
-        .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-        });
-    return result;
-}
-//document.getElementById('host').value = findGetParameter("host");
-</script>
-
-<script src = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.2.min.js"></script>
-<script>
-  function getCurrentWeather() {
-    const api_key = "2c9454b4193e9aba28622a2603bc5f71";
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Dearborn&APPID=" + api_key, function(json){
-      document.getElementById("weatherjson").innerHTML = JSON.stringify(json);
-    });
-  }
-</script>
-
-<a href="parksmart.php">Reset</a><br>
-
-<?php
-
-if (array_key_exists("host", $_GET) && $_GET["host"] !== "") {
-    $servername = $_GET["host"];
-    $username = "ParkSmart";
-    $password = NULL;
-    $dbname = "parksmartdb";
-    // create database connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    //check connection
-    if ($conn->connect_error) {
-        die("Connection failed: ".$conn->connect_error);
-    }
-    echo "Connection successful";
-
-    echo '<table>';
-    echo "<tr><td>Space Index</td><td>IsOccupied?</td><td>Confidence</td><td>Last Update</td></tr>";
-
-    //perform a query on the table
-    $sql = "SELECT * FROM Lot_D";
-    if (isset($_GET["history"])) {
-        $sql .= " FOR SYSTEM_TIME ALL"; 
-    }
-    $result = $conn->query($sql . " ORDER BY Space");
-
-    //if there are any rows returned, print them on the web page.
-    if($result->num_rows > 0) {
-        //output each row
-        while($row = $result->fetch_assoc()) {
-            echo "<tr><td>${row["Space"]}</td><td>${row["IsOccupied"]}</td><td>${row["Confidence"]}</td><td>${row["start_timestamp"]}</td></tr>";
+    <script type="text/babel">
+      class Hello extends React.Component {
+        render() {
+          return <h1>Hello World!</h1>
         }
-    }
-}
-echo '</table>'
-?>
-<p id="weather"></p>
-<br>
-<p id="parking"></p>
-<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.2.min.js"></script>
-<script src="weather.js"></script>
-<script src="pull.js"></script>
-<script>
-  function handle_weather(weather_data) {
+      }
+    </script>
 
-  }
-
-  get_Dearborn_weather().then((val) => {
-    $("#weather").text(JSON.stringify(val));
-    if (parseInt(val.visibility) < 16000) {
-      alert("visibility is poor, results may be inaccurate.");
-    }
-  });
-  pull('Lot_D').then((resp) => {
-    $("#parking").text(JSON.stringify(resp));
-  });
-
-</script>
-</body>
+  </body>
 </html>
