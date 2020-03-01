a8d827ce00107b0092e1a0c3b0a48ad43129a4cc
Bryan Brauchler <bbrauchl@umich.edu>
2020-03-01 16:08:02 -0500

    Updating python demo to work with reactjs app

--- demos/simple-json-request.py
+++ demos/simple-json-request.py
@@ -5,13 +5,28 @@ import ParkSmart
 import random
 
 
-#test of update API
-print('Test of update API')
+#test of update_single API
+print('Test of update_single API')
 print('Updating space 0')
-for i in range(85):
-    resp = ParkSmart.update(Lot='Lot_D', Space=i, IsOccupied=True if random.randint(0,1) else False, Confidence=random.random(), Type='student' if not i in range(73, 80) else 'handicap', debug=False)
 
+resp = ParkSmart.update_single(Lot='Lot_D', Space=0, IsOccupied=True if random.randint(0,1) else False, Confidence=random.random(), Type='electric_vehicle', debug=False)
 print(resp.text)
+
+
+#test of update_multi API
+print('Test of update_multi API')
+print('Updating space 1-83')
+multi_update_states = [{
+        'Lot' : 'Lot_D',
+        'Space' : Space,
+        'IsOccupied' : True if random.randint(0,1) else False,
+        'Confidence' : random.random(),
+        'Type' : 'electric_vehicle' if Space in range(0,4) else 'handicap' if Space in range(73, 80) else 'student',
+        } for Space in range(1,84)]
+resp = ParkSmart.update_multi(Lot='Lot_D', states=multi_update_states, debug=False)
+print(resp.text)
+
+
 
 # test of sql call API
 print("")
@@ -25,6 +40,8 @@ except:
 else:
     print('Success!')
 
+
+
 print("")  
 print('printing{} contents of received object:'.format(" some of" if len(current_database) > 6 else ""))
 # get the current state of Parking space 0 (the one that was updated above)
@@ -34,6 +51,8 @@ if len(current_database) > 6:
     print(current_database[-3:])
 else:
     print(current_database)
+
+
 
 #test of the echo api
 print("")
